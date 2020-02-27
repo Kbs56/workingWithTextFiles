05fedd7fa399112ce8cbb3b3d1293dc38264a7ad
Kbs56 <kenny.sheldon99@gmail.com>
2020-02-26 22:51:12 -0600

    Final commit

--- src/testApp.java
+++ src/testApp.java
@@ -5,17 +5,17 @@ public class testApp {
     public static void main(String[] args) throws IOException {
 
         gradeCalc myObj1 = new gradeCalc("data1.txt");
-        System.out.println(myObj1.getAverageGrade());
-        System.out.println(myObj1.getHigh());
-        System.out.println(myObj1.getLow());
-        System.out.println(myObj1.AllLetterGrades());
+        System.out.println("Average of all grades found in file: " + myObj1.getAverageGrade());
+        System.out.println("Highest grade found in file: " + myObj1.getHigh());
+        System.out.println("Lowest grade found in file: " + myObj1.getLow());
+        System.out.println("All letter grades: " + myObj1.AllLetterGrades());
 
         System.out.println("");
 
         gradeCalc myObj2 = new gradeCalc("data2.txt");
-        System.out.println(myObj2.getAverageGrade());
-        System.out.println(myObj2.getHigh());
-        System.out.println(myObj2.getLow());
-        System.out.println(myObj2.AllLetterGrades());
+        System.out.println("Average of all grades found in file: " + myObj2.getAverageGrade());
+        System.out.println("Highest grade found in file: " + myObj2.getHigh());
+        System.out.println("Lowest grade found in file: " + myObj2.getLow());
+        System.out.println("All letter grades: " + myObj2.AllLetterGrades());
     }
 }
